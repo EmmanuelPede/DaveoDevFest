b58bce0c5c920e9b0ce48b77723430d4ff125578
Thibault Gonce <thibault_gonce@ext.connect-tech.sncf>
2022-10-18 17:41:39 +0200

    [defvest-2K22][BACK] Delete properties files by profil and useless appConfig

--- Back/src/main/java/com/daveo/spring/restapi/mongodb/config/AppConfig.java
+++ Back/src/main/java/com/daveo/spring/restapi/mongodb/config/AppConfig.java
@@ -1,39 +1,10 @@
 package com.daveo.spring.restapi.mongodb.config;
 
-import java.io.*;
+import org.springframework.context.annotation.Configuration;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Log4j2
 public class AppConfig {
-
-    private final String outputPathDir;
-
-    private final String outputFileName;
-
-    public AppConfig(
-            @Value("${watcher.audiosurf.output.path:}") final String outputPathDir,
-            @Value("${watcher.audiosurf.output.filename}") final String outputFileName
-    ) {
-        this.outputPathDir = outputPathDir;
-        this.outputFileName = outputFileName;
-    }
-
-    @Bean
-    public BufferedReader bufferedReader() throws IOException {
-        final String ouputDirPathString = System.getProperty("user.home") + this.outputPathDir;
-        log.info("[AS2-TRACKER] creating bufferedReader Bean");
-
-        final File outputFile = new File(ouputDirPathString + outputFileName);
-        if (!outputFile.exists()) {
-            outputFile.getParentFile().mkdirs();
-            outputFile.createNewFile();
-        }
-
-        return new BufferedReader(new FileReader(outputFile));
-    }
 }
